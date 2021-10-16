ef4a7d2473c83bb0f2a71dac0af9f0f209a1105e
Bartosz Karaś <bkaras2112@gmail.com>
2021-10-16 15:25:06 +0200

    3. zmiana 1 linii

--- src/hello.java
+++ src/hello.java
@@ -1,7 +1,7 @@
 public class hello {
     public class main {
         public static void main{String[] args}{
-            System.out.println("hello world");
+            System.out.println("goodbye world");
         }
     }
 }
